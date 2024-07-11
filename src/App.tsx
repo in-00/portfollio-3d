b77958dccaca7a208bd1c170fceea0c20519b186
Vinayak kumar singh <48807751+in-00@users.noreply.github.com>
2024-07-11 16:33:47 +0000

    Initital code push

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+ 
+    <div className="app">
+
+      <h1 className="name">
+        VINAYAK KUMAR SINGH
+      </h1>
+    </div>
+  )
+}
+
+export default App
